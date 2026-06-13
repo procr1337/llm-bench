9a13815691407fe71f07985c5869c0a6f4ad3824
Procr <193802945+procr1337@users.noreply.github.com>
2026-06-13 21:53:37 +0000

    use more RAM

--- start.sh
+++ start.sh
@@ -21,7 +21,7 @@ DOCKER_COMMON=(
   --ipc host
   --shm-size 8g
   --network llm --ip 172.23.0.10
-  --memory 40g
+  --memory 60g
   --ulimit memlock=-1 --ulimit stack=67108864
 
   # Can't afford more DDR5...
